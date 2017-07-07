a6a91b25bc4b429028dd2ab2f3e5503352815b0e
rover <tdunn1248@gmail.com>
2017-07-07 11:18:13 -0700

    slight refactor

--- commands/list.js
+++ commands/list.js
@@ -4,14 +4,20 @@ const list = () => {
   fs.readFile('tasks.json', 'UTF-8', (err, data) => {
     if (err) throw err
     const parsedList = JSON.parse(data)
-    console.log('ID Description')
-    console.log('-- -----------')
+    formatToDoList()
     const toDos = parsedList.todos
     for(var key in toDos) {
-      console.log(key, toDos[key].task)
+      console.log(key, ' ', toDos[key].task)
     }
-    console.log('\n', Object.keys(toDos).length, 'tasks')
+    console.log('\n')
+    console.log(Object.keys(toDos).length, 'tasks')
   })
+}
+
+function formatToDoList() {
+  console.log('\n')
+  console.log('ID  Description')
+  console.log('--  -----------')
 }
 
 module.exports = list
