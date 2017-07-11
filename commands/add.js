d7cc271845ab6fbc3a0c53c33f67065220ca266c
rover <tdunn1248@gmail.com>
2017-07-11 16:52:18 -0700

    finish test

--- commands/add.js
+++ commands/add.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
+const starter = {serialId: 0, todos: {}}
 
 const addTask = (toDo) => {
 
   const file = fs.readFileSync('tasks.json', 'UTF-8', (err, data) => {
     if (err) throw error
-    fs.writeFile('tasks.json', JSON.stringify(starter), (err) => {
-      if (err) throw err
-    })
+    console.log('--->', data);
+    if (!data) {
+      fs.writeFile('tasks.json', JSON.stringify(starter), (err) => {
+        if (err) throw err
+      })
+    }
     return data
   })
 
@@ -17,11 +21,12 @@ const addTask = (toDo) => {
     complete: false
   }
 
-  parsed.todos[parsed.serialId] = item
-  console.log('Created task ' + parsed.serialId + '.')
   parsed.serialId++
+  parsed.todos[parsed.serialId] = item
 
   const updatedFile = JSON.stringify(parsed, null, 2)
+
+  console.log('Created task ' + parsed.serialId + '.');
 
   fs.writeFile('tasks.json', updatedFile, (err) => {
     if (err) throw err
